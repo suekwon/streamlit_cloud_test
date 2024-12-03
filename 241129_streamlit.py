c8500dae3b9cd23eec45a5743b3c0814781ffaaa
Sue-Jeong, Kwon <sue11@snu.ac.kr>
2024-12-03 14:09:06 +0900

    streamlit cloud test1

--- 241129_streamlit.py
+++ 241129_streamlit.py
@@ -1,5 +1,9 @@
+# %%
 import streamlit as st
 import pandas as pd
+from mistralai import Mistral, UserMessage
+import os
+
 
 st.title("첫 스트림릿 예제")
 st.write('안녕하세요, 처음 만드는 스트림릿 예제입니다.')
@@ -29,3 +33,36 @@ st.slider("슬라이더를 테스트합니다.", 0, 100, 50)
 st.button("버튼을 눌러보세요.")
 st.checkbox("체크박스를 테스트합니다.")
 st.radio("라디오 버튼을 테스트합니다.", ["옵션1", "옵션2", "옵션3"])
+
+
+secret_key = os.getenv("MISTRAL_API_KEY")
+client = Mistral(secret_key)
+model = "mistral-large-latest"
+
+# 채팅 메시지 준비
+# messages = [
+#     {"role": "user",
+#      "content": "파이썬은 정말 재미있는 언어야. 를 영어로 번역해줘."},
+# ]
+
+# API 호출
+chat_response = client.chat.complete(
+    model=model,
+    messages=[
+        {
+            "role": "user",
+            "content": "고양이: 동물 오렌지: 과일 토마토: 채소 비둘기: ",
+        },
+        {
+            "role": "user",
+            "content": "파이썬은 재밌어를 영어로 변역해줘.",
+        },
+    ]
+)
+
+
+st.write(chat_response.choices[0].message.content)
+
+# 사용자가 그만둔다는 어떤 문자를 입력하기 전까지 반복해서 사용자와 대화를 나누는 코드를 작성해보세요.
+
+# %%
